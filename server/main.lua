100ff1e552ea80041854716983e59c3cebe5c678
Muhammad Hifzirrohman Syafi'i <hifzi@users.noreply.github.com>
2020-04-01 01:34:30 +0700

    Update main.lua
    update last coords when exiting

--- server/main.lua
+++ server/main.lua
@@ -236,8 +236,12 @@ end)
 AddEventHandler('playerDropped', function(reason)
 	local playerId = source
 	local xPlayer = ESX.GetPlayerFromId(playerId)
-
+	local playerPed = GetPlayerPed(playerId)
+	local playerCoords = GetEntityCoords(playerPed)
+	local playerHeading = ESX.Math.Round(GetEntityHeading(playerPed), 1)
+	local formattedCoords = {x = ESX.Math.Round(playerCoords.x, 1), y = ESX.Math.Round(playerCoords.y, 1), z = ESX.Math.Round(playerCoords.z, 1), heading = playerHeading}
 	if xPlayer then
+		xPlayer.updateCoords(formattedCoords)
 		TriggerEvent('esx:playerDropped', playerId, reason)
 
 		ESX.SavePlayer(xPlayer, function()
