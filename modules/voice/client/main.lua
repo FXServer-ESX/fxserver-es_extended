d0ada4fc5f153093d7803665bc9b475d5a054410
ArkSeyonet <31637812+ArkSeyonet@users.noreply.github.com>
2020-05-19 10:27:03 -0500

    Re-add QoL code and support for custom loading screens
    - Fix Indent For Smallo's code for spawnmanager
    - Re add wait so player model is loaded before loading screen shuts down
    - Re add support for shutting down custom loading screens automatically when player model is loaded
    - Add loading screen off event to trigger HUD elements to display, so that they do not display while loading screen is still active
    - Add event handler for voice for loading screen off

--- modules/voice/client/main.lua
+++ modules/voice/client/main.lua
@@ -3,10 +3,13 @@ local Input = ESX.Modules['input']
 
 self.Init()
 
-ESX.Loop('draw-voice-level',function ()
-	if NetworkIsPlayerTalking(PlayerId()) then
-		self.DrawLevel(41, 128, 185, 255)
-	else
-		self.DrawLevel(185, 185, 185, 255)
-  end
-end,0)
+-- Add loading screen off event handler to wait for loading screen to be off before showing Voice HUD (ArkSeyonet)
+AddEventHandler('esx:loadingScreenOff', function()
+	ESX.Loop('draw-voice-level',function ()
+		if NetworkIsPlayerTalking(PlayerId()) then
+			self.DrawLevel(41, 128, 185, 255)
+		else
+			self.DrawLevel(185, 185, 185, 255)
+		end
+	end,0)
+end)
