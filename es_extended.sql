b226d36a054e1323465163b2dfe85b48fdf9852d
Sezon <sezon.dev@gmail.com>
2020-06-01 14:18:26 +0200

    Debug 01/06/20 sql error

--- es_extended.sql
+++ es_extended.sql
@@ -1,1 +1,43 @@
 CREATE DATABASE IF NOT EXISTS `es_extended`;
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for `accounts`
+-- ----------------------------
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE `accounts` (
+  `name` varchar(255) NOT NULL,
+  `owner` varchar(64) DEFAULT NULL,
+  `money` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of accounts
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `datastores`
+-- ----------------------------
+DROP TABLE IF EXISTS `datastores`;
+CREATE TABLE `datastores` (
+  `name` varchar(255) NOT NULL,
+  `owner` varchar(64) DEFAULT NULL,
+  `data` longtext,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of datastores
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `inventories`
+-- ----------------------------
+DROP TABLE IF EXISTS `inventories`;
+CREATE TABLE `inventories` (
+  `name` varchar(255) NOT NULL,
+  `owner` varchar(64) DEFAULT NULL,
+  `items` longtext,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
