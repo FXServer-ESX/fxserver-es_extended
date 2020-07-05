bf81791b24a4219bcecb38b0e3d82a765ba1a6d5
Matias Salas <42161973+Dislaik@users.noreply.github.com>
2020-07-05 00:40:51 -0400

    Update main.lua

--- modules/__core__/player/client/main.lua
+++ modules/__core__/player/client/main.lua
@@ -21,3 +21,31 @@ interval = ESX.SetInterval(100, function()
 
 end)
 
+Citizen.CreateThread(function()
+	local isDead = false
+
+	while true do
+		Citizen.Wait(0)
+		local player = PlayerId()
+
+		if NetworkIsPlayerActive(player) then
+			local playerPed = PlayerPedId()
+
+			if IsPedFatallyInjured(playerPed) and not isDead then
+				isDead = true
+
+				local killerEntity, deathCause = GetPedSourceOfDeath(playerPed), GetPedCauseOfDeath(playerPed)
+				local killerClientId = NetworkGetPlayerIndexFromPed(killerEntity)
+
+				if killerEntity ~= playerPed and killerClientId and NetworkIsPlayerActive(killerClientId) then
+					Player:PlayerKilledByPlayer(GetPlayerServerId(killerClientId), killerClientId, deathCause)
+				else
+					Player:PlayerKilled(deathCause)
+				end
+
+			elseif not IsPedFatallyInjured(playerPed) then
+				isDead = false
+			end
+		end
+	end
+end)
