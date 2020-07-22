c6de3a9a643106f8747012b55ddc6233562191f1
Van Damme N <van.damme.nick@telenet.be>
2020-07-22 13:31:16 +0200

    dutch locales
    - update all locales

---
+++ modules/accessories/data/locales/nl.lua
@@ -0,0 +1,31 @@
+-- Copyright (c) Jérémie N'gadi
+--
+-- All rights reserved.
+--
+-- Even if 'All rights reserved' is very clear :
+--
+--   You shall not use any piece of this software in a commercial product / service
+--   You shall not resell this software
+--   You shall not provide any facility to install this particular software in a commercial product / service
+--   If you redistribute this software, you must link to ORIGINAL repository at https://github.com/ESX-Org/es_extended
+--   This copyright should appear in every part of the project code
+
+Translations = {
+  ['valid_purchase'] = 'Aankoop bevestigen?',
+  ['yes'] = 'Ja (<span style="color: green;">$%s</span>)',
+  ['no'] = 'Nee',
+  ['helmet'] = 'Helm / pet',
+  ['glasses'] = 'Brillen',
+  ['mask'] = 'Maskers',
+  ['ears'] = 'Oor accessories',
+  ['shop'] = '%s winkel',
+  ['set_unset'] = 'Doe aan / doe uit',
+  ['not_enough_money'] = 'Je hebt niet genoeg geld.',
+  ['press_access'] = 'Druk op ~INPUT_CONTEXT~ om het menu te openen',
+  ['accessories_blip'] = 'Accessoires',
+  ['no_ears'] = 'Je hebt geen oor accessoires',
+  ['no_glasses'] = 'Je hebt geen brillem',
+  ['no_helmet'] = 'Je hebt geen helm',
+  ['no_mask'] = 'Je hebt geen maskers',
+  ['you_paid'] = 'Je betaalde ~g~$%s~s~',
+}
