a55bf2ad3e49cc2a20dfba573d631fa8f1a9da91
Matias Salas <42161973+Dislaik@users.noreply.github.com>
2020-07-31 09:03:38 -0400

    Fixed DrawMarker and add test menu (#1)
    * Fixed DrawMarker and add test menu
    
    * Update module.lua
    
    * Update module.lua

--- modules/__base__/clotheshop/client/module.lua
+++ modules/__base__/clotheshop/client/module.lua
@@ -13,7 +13,7 @@
 -- Locals
 local Input = M('input')
 local Interact = M('interact')
-local Menu = M('ui.menu')
+M('ui.menu')
 
 -- Properties
 module.Config = run('data/config.lua', {vector3 = vector3})['Config']
@@ -26,62 +26,75 @@ module.Init = function()
   LoadLocale('clotheshop', Config.Locale, translations)
 
   for k, v in pairs(module.Config.Shops) do
-    for i = 1, #v, 1 do
 
-      local key = 'clotheshop:' .. k .. ':' .. i
+    local key = 'clotheshop:' .. k
 
-      Interact.Register({
-        name = key,
-        type = 'marker',
-        distance = module.Config.DrawDistance,
-        radius = 2.0,
-        pos = v[i],
-        size = module.Config.Size.z,
-        mtype = module.Config.Type,
-        color = module.Config.Color,
-        rotate = true,
-        clotheshop = "Clotheshop"
-      })
+    Interact.Register({
+      name = key,
+      type = 'marker',
+      distance = module.Config.DrawDistance,
+      radius = 2.0,
+      pos = v,
+      size = module.Config.Size.z,
+      mtype = module.Config.Type,
+      color = module.Config.Color,
+      rotate = true,
+      clotheshop = k
+    })
 
-      on('esx:interact:enter:' .. key, function(data)
+    on('esx:interact:enter:' .. key, function(data)
 
+    Interact.ShowHelpNotification(_U('clotheshop:press_menu'))
+
+    module.CurrentAction = function()
+      module.OpenClotheShopMenu()
+    end
+
+    end)
+
+    on('esx:interact:exit:' .. key, function(data) 
+      module.CurrentAction = nil 
+      Interact.StopHelpNotification()
+    end)
+  end
+
+	for k,v in pairs(module.Config.Shops) do
+		if v ~= nil then
+			local blip = AddBlipForCoord(v)
+
+			SetBlipSprite(blip, 73)
+			SetBlipDisplay(blip, 4)
+			SetBlipScale(blip, 1.0)
+      SetBlipColour(blip, 47)
+      SetBlipAsShortRange(blip, true)
+
+			BeginTextCommandSetBlipName("STRING")
+			AddTextComponentString(_U('clotheshop:clothes'))
+			EndTextCommandSetBlipName(blip)
+		end
+  end
+end
+
+module.OpenClotheShopMenu = function()
+  local menu = Menu('clotheshop', {
+    float = 'top|left',
+    title = 'Cloteshop Menu',
+    items = {
+      {name = 'close', label = 'Close', type = 'button'},
+    }
+  })
+
+  menu:on('item.click', function(item, index)
+    if item.name == 'close' then
+      menu:destroy()
+      
       Interact.ShowHelpNotification(_U('clotheshop:press_menu'))
 
       module.CurrentAction = function()
         module.OpenClotheShopMenu()
       end
-
-      end)
-
-      on('esx:interact:exit:' .. key, function(data) 
-        module.CurrentAction = nil 
-        Interact.StopHelpNotification()
-      end)
-
     end
-  end
-
-	for k,v in pairs(module.Config.Shops) do
-		if v ~= nil then
-			for i=1, #v, 1 do
-				local blip = AddBlipForCoord(v[i])
-
-				SetBlipSprite(blip, 73)
-				SetBlipDisplay(blip, 4)
-				SetBlipScale(blip, 1.0)
-				SetBlipColour(blip, 80)
-
-				BeginTextCommandSetBlipName("STRING")
-				AddTextComponentString(_U('clotheshop:clothes'))
-				EndTextCommandSetBlipName(blip)
-			end
-		end
-  end
-
-end
-
-module.OpenClotheShopMenu = function()
-  -- TODO: Create Shop Menu
+  end)
 end
 
 module.RegisterControls = function()
