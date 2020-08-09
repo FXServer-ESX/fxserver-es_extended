eaec142339678802c1991be8880e0e484d2d3c46
Abdulkadir AKTAŞ <abdulkadir.aktas@hotmail.com>
2020-08-09 12:44:48 +0000

    Update en.lua

--- modules/__user__/legacyfuel/data/locales/en.lua
+++ modules/__user__/legacyfuel/data/locales/en.lua
@@ -1,1 +1,9 @@
-[]
+Translations = {
+    ['fuelLabel'] = "Press ~g~G ~w~to cancel the fueling of your vehicle. $~r~ %s ~w~+  tax",
+    ['needcarexit'] = "Benzini doldurmak için araçtan çıkın.",
+    ['fueliffull'] = 'Vehicle is too filled with gas to be fueled',
+    ['notenoughmoney'] = "You currently don't have enough money on you to buy fuel with",
+    ['fuelvehicle'] = 'Press ~g~G ~w~to fuel your vehicle. $~r~0.5/~w~gallon + tax',
+    ['carryCanItem'] = 'Press ~g~G ~w~to cancel fueling the vehicle. Currently at: %s % - Jerry Can: %s',
+    ['carryCanItemLabel'] = 'Press ~g~G ~w~to fuel the vehicle with your gas can'
+}
