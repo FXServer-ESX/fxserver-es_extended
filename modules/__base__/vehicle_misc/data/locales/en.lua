c1052f343968562175155788ffe9c74386b0aaff
Bastiti <37415063+Bastiti@users.noreply.github.com>
2020-11-07 15:31:29 +0100

    vehicle_misc with refueling at gas station

---
+++ modules/__base__/vehicle_misc/data/locales/en.lua
@@ -0,0 +1,6 @@
+Translations = {
+    --General
+    ['Fuel_Loaded'] = 'Fuel Module had been initialized.'
+    ['Leave_car'] = 'Gas Station: Please turn off your engine and leave your vehicle!'
+    ['choose_fuel'] = 'Gas Station: Press  ~INPUT_CONTEXT~ to choose your fuel!'
+}
