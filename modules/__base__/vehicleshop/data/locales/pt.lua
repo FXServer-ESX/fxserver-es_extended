6404e7ede2a792eb4e57fa44cec2cbf91af776db
nrcsantos <nrcsantos@me.com>
2021-02-11 10:26:01 +0100

    Create pt.lua
    Portuguese/Portugal language file. If the 'locale' is set to 'pt' on the main configuration, some modules will reproduce an error on the absence of this translations.

---
+++ modules/__base__/vehicleshop/data/locales/pt.lua
@@ -0,0 +1,43 @@
+-- Copyright (c) Jérémie N'gadi
+--
+-- All rights reserved.
+--
+-- Even if 'All rights reserved' is very clear :
+--
+--   You shall not use any piece of this software in a commercial product / service
+--   You shall not resell this software
+--   You shall not provide any facility to install this particular software in a commercial product / service
+--   If you redistribute this software, you must link to ORIGINAL repository at https://github.com/ESX-Org/es_extended
+--   This copyright should appear in every part of the project code
+
+Translations = {
+  ['blip_buy_title'] = 'Concessionário de Carros (Comprar)',
+  ['blip_sell_title'] = 'Concessionário de Carros (Vender)',
+  ['press_to_sell'] = 'Pressione ~INPUT_CONTEXT~ para vender este veículo.',
+  ['must_be_in_vehicle'] = 'Você deverá estar no veículo para usar isto.',
+  ['already_in_vehicle'] = 'Você já se encontra dentro de um veículo.',
+  ['shop_title'] = 'Loja de Veículos',
+  ['test_drive'] = 'Efectuar um teste',
+  ['buy_confirm'] = 'Comprar %s por $%s?',
+  ['generate_failure'] = 'Erro ao gerar a matrícula. Por favor contacto o administrador do servidor.',
+  ['buy_success'] = 'Você comprou um ~y~%s~s~ com a matrícula ~b~%s~s~ por ~g~$%s',
+  ['test_drive_started'] = 'Iniciou o teste de condução.',
+  ['test_drive_remaining_long'] = '~INPUT_CONTEXT~ Para terminar antecipadamente - Tempo restante: ~g~%s',
+  ['test_drive_remaining_med'] = '~INPUT_CONTEXT~Para terminar antecipadamente - Tempo restante: ~y~%s',
+  ['test_drive_remaining_short'] = '~INPUT_CONTEXT~ Para terminar antecipadamente - Tempo restante: ~r~%s',
+  ['end_test_drive_death'] = 'O teste de condução terminou devido a uma fatalidade.',
+  ['test_drive_ended'] = 'Terminou o teste de condução.',
+  ['top_speed'] = 'Velocidade Máxima',
+  ['acceleration'] = 'Aceleração',
+  ['gears'] = 'Mudanças',
+  ['seating_capacity'] = 'Lugares',
+  ['sell_success'] = 'Você vendeu o seu ~y~%s~s~ com a matrícula ~b~%s~s~ por ~g~$%s',
+  ['must_own_vehicle'] = '~r~Você deve ser o dono deste veículo para poder utilizar este marcador.',
+  ['model_loading'] = 'Por favor aguarde para este modelo carregar...',
+  ['server_buy_success'] = '^7[^4%s^7 |^5 %s %s^7] ^3comprou^7: ^5%s^7 com a matrícula ^3%s ^7por ^2$%s^7',
+  ['server_buy_failure'] = '^1Erro ao comprar o veículo. Por favor contacto o administrador do servidor.^7',
+  ['server_sell_success'] = '^7[^4%s^7 |^5 %s %s^7] ^vendido^7: ^5%s^7 com a matrícula ^3%s ^7por ^2$%s^7',
+  ['server_sell_failure'] = '^1Erro ao vender o veículo. Por favor contacto o administrador do servidor.^7',
+  ['press_access'] = 'Pressione ~INPUT_CONTEXT~ para aceder à Loja de Veículos.',
+  ['shop_being_used'] = 'Alguém já se encontra a utilizar a loja. Aguarde por favor que termine.'
+}
