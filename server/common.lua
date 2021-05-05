494b82b469997ec7d04f45ea671b13ae93648ac7
MiddleSkillz <MiddleSkillz@gmail.com>
2021-05-05 09:08:20 -0600

    ghmattimysql convert part 2

--- server/common.lua
+++ server/common.lua
@@ -18,41 +18,71 @@ function getSharedObject()
 	return ESX
 end
 
-MySQL.ready(function()
-	MySQL.Async.fetchAll('SELECT * FROM items', {}, function(result)
-		for k,v in ipairs(result) do
-			ESX.Items[v.name] = {
-				label = v.label,
-				weight = v.weight,
-				rare = v.rare,
-				canRemove = v.can_remove
-			}
-		end
-	end)
-
-	MySQL.Async.fetchAll('SELECT * FROM jobs', {}, function(jobs)
-		for k,v in ipairs(jobs) do
-			ESX.Jobs[v.name] = v
-			ESX.Jobs[v.name].grades = {}
-		end
-
-		MySQL.Async.fetchAll('SELECT * FROM job_grades', {}, function(jobGrades)
-			for k,v in ipairs(jobGrades) do
-				if ESX.Jobs[v.job_name] then
-					ESX.Jobs[v.job_name].grades[tostring(v.grade)] = v
-				else
-					print(('[^5es_extended^0] [^3WARNING^7] Ignoring job grades for ^5"%s"^0 due to missing job'):format(v.job_name))
-				end
-			end
-
-			for k2,v2 in pairs(ESX.Jobs) do
-				if ESX.Table.SizeOf(v2.grades) == 0 then
-					ESX.Jobs[v2.name] = nil
-					print(('[^5es_extended^0] [^3WARNING^7] Ignoring job ^5"%s"^0due to no job grades found'):format(v2.name))
-				end
+ESX.Ready(function()
+	if Config.UseMySQLAsync then
+		MySQL.Async.fetchAll('SELECT * FROM items', {}, function(result)
+			for k,v in ipairs(result) do
+				ESX.Items[v.name] = {
+					label = v.label,
+					weight = v.weight,
+					rare = v.rare,
+					canRemove = v.can_remove
+				}
 			end
 		end)
-	end)
+	
+		MySQL.Async.fetchAll('SELECT * FROM jobs', {}, function(jobs)
+			for k,v in ipairs(jobs) do
+				ESX.Jobs[v.name] = v
+				ESX.Jobs[v.name].grades = {}
+			end
+	
+			MySQL.Async.fetchAll('SELECT * FROM job_grades', {}, function(jobGrades)
+				for k,v in ipairs(jobGrades) do
+					if ESX.Jobs[v.job_name] then
+						ESX.Jobs[v.job_name].grades[tostring(v.grade)] = v
+					else
+						print(('[^5es_extended^0] [^3WARNING^7] Ignoring job grades for ^5"%s"^0 due to missing job'):format(v.job_name))
+					end
+				end
+			end)
+		end)
+	else
+		exports.ghmattimysql:execute('SELECT * FROM items', {}, function(result)
+			for k,v in ipairs(result) do
+				ESX.Items[v.name] = {
+					label = v.label,
+					weight = v.weight,
+					rare = v.rare,
+					canRemove = v.can_remove
+				}
+			end
+		end)
+	
+		exports.ghmattimysql:execute('SELECT * FROM jobs', {}, function(jobs)
+			for k,v in ipairs(jobs) do
+				ESX.Jobs[v.name] = v
+				ESX.Jobs[v.name].grades = {}
+			end
+	
+			exports.ghmattimysql:execute('SELECT * FROM job_grades', {}, function(jobGrades)
+				for k,v in ipairs(jobGrades) do
+					if ESX.Jobs[v.job_name] then
+						ESX.Jobs[v.job_name].grades[tostring(v.grade)] = v
+					else
+						print(('[^5es_extended^0] [^3WARNING^7] Ignoring job grades for ^5"%s"^0 due to missing job'):format(v.job_name))
+					end
+				end
+			end)
+		end)
+	end
+
+	for k2,v2 in pairs(ESX.Jobs) do
+		if ESX.Table.SizeOf(v2.grades) == 0 then
+			ESX.Jobs[v2.name] = nil
+			print(('[^5es_extended^0] [^3WARNING^7] Ignoring job ^5"%s"^0due to no job grades found'):format(v2.name))
+		end
+	end
 
 	print('[^5es_extended^0] [^2INFO^7] ESX ^5Legacy^0 initialized')
 end)
