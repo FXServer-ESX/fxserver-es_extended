e386f9184bd74e228c82c9ca223bec113cbe6cfb
Divine <diviy2k@gmail.com>
2021-05-07 19:09:15 +0100

    feat(base/carwash):Added carwash module

---
+++ modules/__base__/carwash/data/config.lua
@@ -0,0 +1,65 @@
+-- Copyright (c) Jérémie N'gadi
+--
+-- All rights reserved.
+--
+-- Even if 'All rights reserved' is very clear :
+--
+--   You shall not use any piece of this software in a commercial product / service
+--   You shall not resell this software
+--   You shall not provide any facility to install this particular software in a commercial product / service
+--   If you redistribute this software, you must link to ORIGINAL repository at https://github.com/ESX-Org/es_extended
+--   This copyright should appear in every part of the project code
+
+Config = {}
+
+Config.DrawDistance = 200
+
+Config.Price = 1
+
+Config.CarWashZones = {
+
+  Strawberry = {
+    Pos   = vector3(26.5906,  -1392.0261,  28.3634),
+    Size  = {x = 3.0, y = 3.0, z = 1.5},
+    Type  = 27,
+    Color = {r = 0, g = 255, b = 0, a = 225}
+  },
+
+
+  Davis1 = {
+    Pos   = vector3(170.77,  -1718.44,  28.3316),
+    Size  = {x = 3.0, y = 3.0, z = 1.5},
+    Type  = 27,
+    Color = {r = 0, g = 255, b = 0, a = 225}
+  },
+
+  Davis2 = {
+    Pos   = vector3(167.93,  -1715.48,  28.3316),
+    Size  = {x = 3.0, y = 3.0, z = 1.5},
+    Type  = 27,
+    Color = {r = 0, g = 255, b = 0, a = 225}
+  },
+
+  Paleto = {
+    Pos   = vector3(-74.5693,  6427.8715,  30.4400),
+    Size  = {x = 3.0, y = 3.0, z = 1.5},
+    Type  = 27,
+    Color = {r = 0, g = 255, b = 0, a = 225}
+  },
+
+  LittlSeoul = {
+    Pos   = vector3(-699.6325,  -932.7043,  18.0139),
+    Size  = {x = 3.0, y = 3.0, z = 1.5},
+    Type  = 27,
+    Color = {r = 0, g = 255, b = 0, a = 225}
+  },
+
+  SandyShores = {
+    Pos   = vector3(1362.5385, 3592.1274, 33.95),
+    Size  = {x = 3.0, y = 3.0, z = 1.5},
+    Type  = 27,
+    Color = {r = 0, g = 255, b = 0, a = 225}
+  },
+
+}
+
