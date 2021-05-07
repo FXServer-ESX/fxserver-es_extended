2b066283dd903ebe62a2b52ff3d309dc19f5c828
Divine <diviy2k@gmail.com>
2021-05-07 03:27:01 +0100

    Carwash module

---
+++ modules/__base__/carwash/data/locales/en.lua
@@ -0,0 +1,20 @@
+
+-- Copyright (c) Jérémie N'gadi
+--
+-- All rights reserved.
+--
+-- Even if 'All rights reserved' is very clear :
+--
+--   You shall not use any piece of this software in a commercial product / service
+--   You shall not resell this software
+--   You shall not provide any facility to install this particular software in a commercial product / service
+--   If you redistribute this software, you must link to ORIGINAL repository at https://github.com/ESX-Org/es_extended
+--   This copyright should appear in every part of the project code
+
+Translations = {
+    -- General
+    ['press_to_wash'] = 'Press ~INPUT_CONTEXT~ to wash your vehicle.',
+    ['car_washed'] = 'The car has just been washed.',
+    ['not_enough_money'] = 'You don\'t have enough money',
+    ['must_be_in_vehicle'] = '~r~You must be in a vehicle to use this.'
+}
