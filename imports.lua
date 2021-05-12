f7b0668c87c1176d87fbf7424ff28a36d0d02d7a
Linden <65407488+thelindat@users.noreply.github.com>
2021-05-13 03:29:06 +1000

    Setup patching for MySQL functions when using ghmattimysql (incomplete)

--- imports.lua
+++ imports.lua
@@ -7,4 +7,18 @@ if not IsDuplicityVersion() then -- Only register this event for the client
 			if OnPlayerData ~= nil then OnPlayerData(key, val) end
 		end
 	end)
+elseif MySQL and not ESX.GetConfig().UseMySQLAsync then
+	Citizen.CreateThread(function()
+		function MySQL.ready(callback)
+			Citizen.CreateThread(function ()
+				while GetResourceState('ghmattimysql') ~= 'started' do
+					Citizen.Wait(0)
+				end
+				MySQL.Async.execute		= function(query, values, cb) exports.ghmattimysql:execute(query, values, cb) end
+				MySQL.Async.fetchAll	= function(query, values, cb) exports.ghmattimysql:execute(query, values, cb) end
+				MySQL.Async.fetchScalar	= function(query, values, cb) exports.ghmattimysql:scalar(query, values, cb) end
+				callback()
+			end)
+		end
+	end)
 end
